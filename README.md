d8390ae123f5b7eb0e56a7be7a38de96be333921
Sohyun Lee <sohyunc1990@gmail.com>
2020-06-26 12:20:22 +0000

    Automatically backed up by Learn

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Beatles Loops
+a# Beatles Loops
 
 ## Objectives
 
--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// add solution here
+var Beatles = ['John Lennon plays Guitar', 'Paul McCartney plays Bass Guitar', 'George Harrison plays Lead Guitar', 'Ringo Starr plays Drums'];
+
+const facts =  [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"]
+function johnLennonFacts() {
+  var text = [];
+  var i = 0;
+  while(i <facts.length) {
+     text.push(facts[i] + "!!!");
+     i++;
+  }
+ return text;
+ 
+}
+
+console.log(johnLennonFacts());
